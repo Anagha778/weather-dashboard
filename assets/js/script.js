8ee06d343711cbf0207100e74eaa6ae8a7dbfcfc
anagha778 <anagha778@gmail.com>
2021-01-16 21:55:14 -0500

    click on dynamic links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
-
 //Global variables
 var cities =[];
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#search");
 var citiesEl = document.querySelector(".cities-list");
+var searchbtnEl = document.querySelector("#searchbtn");
+var GUVIndex= 0;
+var uvbodyEl = document.createElement("div");
+var weather5El = document.querySelector(".weather5"); 
+var citieslistEl = document.getElementsByTagName("li");
+var weatherrowEl = document.querySelector(".weather-row");
+var forecastEl = document.querySelector(".forecast-row");
 
+
+//function to display weather data on click of links
+function dynamicEvent(){
+    var city = this.textContent.trim();
+    clearNodes();
+    gettodayWeatherDetails(city);
+  };
+  
 
 //Search city
 var searchCity = function(event)
