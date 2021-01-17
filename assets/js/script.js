f7476839f8f82b1d391b19868f190140520052ee
anagha778 <anagha778@gmail.com>
2021-01-16 21:58:13 -0500

    current day weather data display

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,89 @@ var citieslistEl = document.getElementsByTagName("li");
 var weatherrowEl = document.querySelector(".weather-row");
 var forecastEl = document.querySelector(".forecast-row");
 
+
+//Display todays weather data
+function displayWeather(data,cityname)
+{
+  // check if api returned any city
+  if (data.length === 0)
+   {
+    weatherrowEl.textContent = "No data found for city: "+cityname;
+    return;
+  }  
+  
+  var latvar = 0;
+  var lonvar = 0;
+  var tempvar = 0;
+  var humidityvar = 0;
+  var windvar = 0;
+  var datevar =0;
+  var iconvar = "";
+  var iconurl="";
+  var uvvar = 0;
+  var uppercity ="";
+  var dateString="";
+
+            latvar = data.coord.lat;
+            lonvar = data.coord.lon;
+            tempvar = data.main.temp;
+            humidityvar = data.main.humidity;
+            windvar = data.wind.speed;
+            iconvar = data.weather[0].icon;
+            iconurl = "https:///openweathermap.org/img/w/" + iconvar + ".png";
+            dateString = data.dt;
+            datevar = moment.unix(dateString).format("MM/DD/YYYY");
+            uppercity = cityname.charAt(0).toUpperCase()+cityname.slice(1);
+
+            //create card for weather data           
+
+          var weathercardEl = document.createElement("div");
+          weathercardEl.classList = "col-md-12 card weather-card";
+          weatherrowEl.appendChild(weathercardEl);
+            //create card body div
+          var cardbodyEl = document.createElement("div");
+          cardbodyEl.classList = "card-body";
+          weathercardEl.appendChild(cardbodyEl);
+          //add body contents
+          var cardtitleEl = document.createElement("h3");
+          cardtitleEl.classList = "card-title";
+          cardtitleEl.innerHTML = uppercity +" "+datevar;
+          
+          
+          var imgvar = document.createElement("img");
+          imgvar.classList = "card-title";
+          imgvar.setAttribute("src",iconurl);
+          cardtitleEl.appendChild(imgvar);
+          cardbodyEl.appendChild(cardtitleEl);
+
+          var tempbodyEl = document.createElement("div");
+          tempbodyEl.classList="card-text";
+          tempbodyEl.innerHTML = "Temperature: "+tempvar+"&#8457";
+          cardbodyEl.appendChild(tempbodyEl);
+
+          var humiditybodyEl = document.createElement("div");
+          humiditybodyEl.classList="card-text";
+          humiditybodyEl.innerHTML = "Humidity: "+humidityvar+"%";
+          cardbodyEl.appendChild(humiditybodyEl);
+
+          var windbodyEl = document.createElement("div");
+          windbodyEl.classList="card-text";
+          windbodyEl.innerHTML  = "Wind Speed: "+windvar+" MPH";
+          cardbodyEl.appendChild(windbodyEl);
+            //get UV index
+          
+          getUVI(latvar,lonvar,dateString);  
+          
+          //add 5 days forecast header and data
+          cardbodyEl.appendChild(uvbodyEl);
+          weather5El.innerHTML="5-Day Forecast:";
+          getfivedayWeatherDetails(cityname);
+          
+          //Add to local repo
+          addTorepository(cityname);     
+};
+
+
 //function to get todays temperature
 var gettodayWeatherDetails = function(cityname){
     
